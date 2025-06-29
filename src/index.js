0aff93a96ad7517613d37dccae6e0320b67d2351
sanchirjav <sanchirjav.lkhasuren@gmail.com>
2025-06-29 23:31:40 +0800

    hello

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import GiftBoxAnimation from "./GiftBoxAnimation";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <GiftBoxAnimation />
+  </React.StrictMode>,
+  rootElement
+);
